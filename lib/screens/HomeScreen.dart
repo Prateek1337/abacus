9290d283604cf70aea35c37d2ef400cdb355d52a
sdew021 <sdewangan021021@gmail.com>
2021-01-11 18:29:54 +0530

    added registration functionality

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,12 +1,15 @@
 // import 'package:abacus/screens/LoginScreen.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 
+import 'dart:html';
+
 import 'package:abacus/screens/AdditionSettingScreen.dart';
 import 'package:abacus/screens/MultiplicationSettingScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:abacus/Variables.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -19,9 +22,95 @@ class IsAllowedScreen extends StatelessWidget {
   IsAllowedScreen({this.user});
   @override
   Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    //CollectionReference users = FirebaseFirestore.instance.collection('users');
+    CollectionReference users = FirebaseFirestore.instance.collection('user');
+    //   return FutureBuilder<DocumentSnapshot>(
+    //     future: users.doc("phoneNumberAccess").get(),
+    //     builder:
+    //         (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+    //       if (snapshot.hasError) {
+    //         return Text("Something went wrong");
+    //       }
+
+    //       if (snapshot.connectionState == ConnectionState.done) {
+    //         Map<String, dynamic> data = snapshot.data.data();
+    //         if (data[user] != null && data[user] == true) {
+    //           return HomeScreen(user: user);
+    //         }
+    //         //users.doc("phoneNumberAcccess").set({'$user': false});
+    //         addUser(user);
+    //         return Center(
+    //           child: Wrap(children: <Widget>[
+    //             Card(
+    //               shape: RoundedRectangleBorder(
+    //                 borderRadius: BorderRadius.circular(10.0),
+    //                 side: BorderSide(
+    //                   color: Colors.blue,
+    //                   width: 2.0,
+    //                 ),
+    //               ),
+    //               elevation: 10,
+    //               color: Colors.blue[50],
+    //               child: Padding(
+    //                 padding: const EdgeInsets.all(16.0),
+    //                 child: ListTile(
+    //                   title: Column(
+    //                     children: [
+    //                       Text(
+    //                         "The Phone Number $user is not allowed.\nPlease get your phone number validated. ",
+    //                         style: new TextStyle(
+    //                             fontSize: 24.0,
+    //                             fontWeight: FontWeight.bold,
+    //                             color: Colors.blue),
+    //                       ),
+    //                     ],
+    //                   ),
+    //                 ),
+    //               ),
+    //             ),
+    //           ]),
+    //         );
+
+    //         //   return Text(
+    //         //       "The Phone Number $user is not allowed or invalid. Please get your phone number validated ");
+    //       }
+
+    //       return Center(
+    //         child: Wrap(children: <Widget>[
+    //           Card(
+    //             shape: RoundedRectangleBorder(
+    //               borderRadius: BorderRadius.circular(10.0),
+    //               side: BorderSide(
+    //                 color: Colors.blue,
+    //                 width: 2.0,
+    //               ),
+    //             ),
+    //             elevation: 10,
+    //             color: Colors.blue[50],
+    //             child: Padding(
+    //               padding: const EdgeInsets.all(16.0),
+    //               child: ListTile(
+    //                 title: Column(
+    //                   children: [
+    //                     Text(
+    //                       "LOADING...",
+    //                       style: new TextStyle(
+    //                           fontSize: 24.0,
+    //                           fontWeight: FontWeight.bold,
+    //                           color: Colors.blue),
+    //                     ),
+    //                   ],
+    //                 ),
+    //               ),
+    //             ),
+    //           ),
+    //         ]),
+    //       );
+    //     },
+    //   );
+    //
     return FutureBuilder<DocumentSnapshot>(
-      future: users.doc("phoneNumberAccess").get(),
+      future: users.doc("accessControl").get(),
       builder:
           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
         if (snapshot.hasError) {
@@ -30,9 +119,20 @@ class IsAllowedScreen extends StatelessWidget {
 
         if (snapshot.connectionState == ConnectionState.done) {
           Map<String, dynamic> data = snapshot.data.data();
-          if (data[user] != null && data[user] == true) {
-            return HomeScreen(user: user);
+          //return Text(data[user]['loggedIn'].toString());
+          if (data[user] != null) {
+            if (data[user]['isAllowed'] != null &&
+                data[user]['isAllowed'] == true &&
+                data[user]['registered'] == false) {
+              return RegistrationScreen(user: user);
+              //return RegisterScreen(user: user);
+            } else if (data[user]['isAllowed'] != null &&
+                data[user]['isAllowed'] == true &&
+                data[user]['registered'] == true) {
+              return HomeScreen(user: user);
+            }
           }
+
           //users.doc("phoneNumberAcccess").set({'$user': false});
           addUser(user);
           return Center(
@@ -66,9 +166,6 @@ class IsAllowedScreen extends StatelessWidget {
               ),
             ]),
           );
-
-          //   return Text(
-          //       "The Phone Number $user is not allowed or invalid. Please get your phone number validated ");
         }
 
         return Center(
@@ -108,8 +205,11 @@ class IsAllowedScreen extends StatelessWidget {
 }
 
 addUser(String phNo) {
-  FirebaseFirestore.instance.collection("users").doc('phoneNumberAccess').set({
-    '$phNo': false,
+  // FirebaseFirestore.instance.collection("users").doc('phoneNumberAccess').set({
+  //   '$phNo': false,
+  // }, SetOptions(merge: true));
+  FirebaseFirestore.instance.collection("user").doc('accessControl').set({
+    '$phNo': {'isAllowed': false, 'registered': false},
   }, SetOptions(merge: true));
 }
 
@@ -137,6 +237,243 @@ addUser(String phNo) {
 
 Future _stop() async {
   if (flutterTts != null) await flutterTts.stop();
+}
+
+class RegistrationScreen extends StatefulWidget {
+  final String user;
+
+  RegistrationScreen({this.user});
+
+  @override
+  _RegistrationScreenState createState() =>
+      new _RegistrationScreenState(user: user);
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final String user;
+  TextEditingController sNameController,
+      fNameController,
+      addressController,
+      emailController,
+      dobController;
+  @override
+  void initState() {
+    super.initState();
+    sNameController = TextEditingController();
+    fNameController = TextEditingController();
+    addressController = TextEditingController();
+    emailController = TextEditingController();
+    dobController = TextEditingController();
+  }
+
+  _RegistrationScreenState({
+    @required this.user,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Submit Details"),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: new TextField(
+                      keyboardType: TextInputType.number,
+                      // inputFormatters: [
+                      //   FilteringTextInputFormatter.digitsOnly,
+                      //   LengthLimitingTextInputFormatter(1),
+                      //   CustomRangeTextInputFormatter3(),
+                      // ],
+                      controller: sNameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          labelText: "Student's Name",
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: new TextField(
+                      keyboardType: TextInputType.number,
+                      // inputFormatters: [
+                      //   FilteringTextInputFormatter.digitsOnly,
+                      //   LengthLimitingTextInputFormatter(1),
+                      //   CustomRangeTextInputFormatter3(),
+                      // ],
+                      controller: fNameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          labelText: "Father's Name",
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: new TextFormField(
+                      //keyboardType: TextInputType.number,
+                      // inputFormatters: [
+                      //   FilteringTextInputFormatter.digitsOnly,
+                      //   LengthLimitingTextInputFormatter(1),
+                      //   CustomRangeTextInputFormatter3(),
+                      // ],
+                      controller: addressController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          labelText: "Address", border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: new TextField(
+                      keyboardType: TextInputType.number,
+                      // inputFormatters: [
+                      //   FilteringTextInputFormatter.digitsOnly,
+                      //   LengthLimitingTextInputFormatter(1),
+                      //   CustomRangeTextInputFormatter3(),
+                      // ],
+                      controller: emailController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          labelText: "Email ID", border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: new TextField(
+                      keyboardType: TextInputType.number,
+                      // inputFormatters: [
+                      //   FilteringTextInputFormatter.digitsOnly,
+                      //   LengthLimitingTextInputFormatter(1),
+                      //   CustomRangeTextInputFormatter3(),
+                      // ],
+                      controller: dobController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          labelText: "Date Of Birth",
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: new RaisedButton(
+                      onPressed: () => {
+                        if (sNameController.text != null &&
+                            fNameController.text != null &&
+                            addressController.text != null &&
+                            dobController.text != null &&
+                            emailController.text != null &&
+                            sNameController.text != "" &&
+                            fNameController.text != "" &&
+                            addressController.text != "" &&
+                            dobController.text != "" &&
+                            emailController.text != "")
+                          {
+                            registerUser(
+                                user,
+                                sNameController.text,
+                                fNameController.text,
+                                addressController.text,
+                                emailController.text,
+                                dobController.text),
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeScreen(
+                                        user: user,
+                                      )),
+                            ),
+                          }
+                        else
+                          {
+                            showToast("Fields cannot be empty", false),
+                          }
+                      },
+                      child: new Text(
+                        'SUBMIT',
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white),
+                      ),
+                      color: Theme.of(context).accentColor,
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//showToast(String s, bool bool) {}
+void showToast(String text, bool isGreen) {
+  Fluttertoast.showToast(
+      msg: text,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: isGreen ? Colors.green : Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
+
+registerUser(String phone, String sName, String fName, String address,
+    String email, String dob) {
+  // FirebaseFirestore.instance.collection("users").doc('phoneNumberAccess').set({
+  //   '$phNo': false,
+  // }, SetOptions(merge: true));
+  FirebaseFirestore.instance.collection("user").doc('registrations').set({
+    '$phone': {
+      'phone': phone,
+      'sName': sName,
+      'fName': fName,
+      'address': address,
+      'email': email,
+      'dob': dob,
+    }
+  }, SetOptions(merge: true)).whenComplete(
+    () => {
+      FirebaseFirestore.instance.collection("user").doc('accessControl').set({
+        '$phone': {'registered': true},
+      }, SetOptions(merge: true))
+    },
+  );
 }
 
 class HomeScreen extends StatefulWidget {
